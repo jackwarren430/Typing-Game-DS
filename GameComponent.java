d6a395a63cb4bdb374972f4177dd99423d17aaa1
jackwarren430 <90930942+jackwarren430@users.noreply.github.com>
2022-03-16 11:52:01 -0400

    making progress

--- GameComponent.java
+++ GameComponent.java
@@ -3,21 +3,56 @@ import java.awt.*;
 import java.awt.geom.*;
 import java.lang.Math;
 
-public class GameComponent extends JComponent{
-	private String[] wordArr = {"blam", "kablooey", "whabul"};
-	private int gameSize;
+import java.io.*;
+import java.util.*;
 
-	public GameComponent(){
-		this.gameSize = wordArr.length;
+public class GameComponent extends JComponent {
+	private static final long serialVersionUID = 0000;
+	private ArrayList<String> wordArr;
+	TyperFrame frame;
+
+	public GameComponent() throws IOException{
+		initWordArr();
+
+		frame = (TyperFrame)this.getTopLevelAncestor();
 	}
 
 	public void paintComponent(Graphics g){
 		Graphics2D pen = (Graphics2D) g;
-		pen.drawString("wham", 400, 200);
-		Container c = this.getTopLevelAncestor();
-		TyperFrame t = (TyperFrame)c;
-		t.getFrameSize();
+		int width = frame.getFrameSize()[0];
+		int height = frame.getFrameSize()[1];
+
+
+		if (frame.getIsHome() && !frame.getGameStart()){
+			paintStartButton(pen);
+		} else if (!frame.getIsHome() && frame.getGameStart()){
+			paintWords(pen);
+		}
+		
+	
 	}
 
+	private void paintWords(Graphics2D pen){
+		for (String word : wordArr){
+			pen.drawString(word, );
+		}
+	}	
+
+	private void paintStartButton(Graphics2D pen){
+
+	}
+
+	public void initWordArr() throws IOException{
+		Scanner scan = new Scanner(new File("dictionary.txt"));
+		hold = new ArrayList<String>();
+		while (scan.hasNextLine()){
+			hold.add(scan.nextLine().strip());
+		}
+
+		for (int i = 0; i < frame.getGameSize(); i++){
+			int randInt = 0 + (int)(Math.random() * ((frame.getGameSize()) + 1));
+			wordArr.add(hold.at(randInt));
+		}
+	}
 
 }
--- TyperFrame.java
+++ TyperFrame.java
@@ -14,10 +14,21 @@ public class TyperFrame extends JFrame{
 	public static final int HEIGHT = 700;
 
 	private JFrame thisFrame = this;
-
+	private JPanel mainPanel;
 	private GameComponent game;
 
+	private Boolean gameStart;
+	private Boolean isHome;
+
+	private int gameSize;
+
 	public TyperFrame(){
+
+		gameStart = false;
+		isHome = true;
+
+		gameSize = 20;
+
 		this.setSize(WIDTH, HEIGHT + 25);
 		this.setTitle("Typing Wizard");
 		addComponents();		
@@ -30,35 +41,42 @@ public class TyperFrame extends JFrame{
   	}
 
   	public void addComponents(){
-
-  		game = new GameComponent();
-  		this.add(game);
-
-		t = new Timer(10, new MovementListener());
+  		mainPanel = new JPanel(new BorderLayout());
+  		configurePanel();
+  		this.add(mainPanel);
+		t = new Timer(500, new MovementListener());
 		this.addMouseMotionListener(new MouseMovementListener());
 		this.addKeyListener(new KeyBoardListener());
   		this.setFocusable(true);
   	}
 
+  	private void configurePanel(){
+  		game = new GameComponent();
+  		mainPanel.add(game, BorderLayout.CENTER);
+  	}
+
   	public int[] getFrameSize(){
-  		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-  		pack(); // Need this, otherwise insets() show as 0.  
+  		//Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+  		Dimension screenSize = getSize();
+  		//pack(); // Need this, otherwise insets() show as 0.  
 		int scrW = (int)screenSize.getWidth();
 		int scrH = (int)screenSize.getHeight();
-		int innerW = scrW - getInsets().left - getInsets().right;
-		int innerH = scrH - getInsets().top - getInsets().bottom;
+		int innerW = scrW; //scrW - getInsets().left - getInsets().right;
+		int innerH = scrH - 22;//scrH - getInsets().top - getInsets().bottom;
   		// Need to setSize(), otherwise pack() will collapse the empty JFrame
-  		setSize(scrW, scrH);
+  		//setSize(scrW, scrH);
   		int[] toReturn = {innerW, innerH};
   		return toReturn;
+
   	}
+
 
   	class MovementListener implements ActionListener{
     	public void actionPerformed(ActionEvent e){
-        	//repaint();
-        	//System.out.println("My mouse moved!");
+        	
     	}
    	}
+
 
   	class MouseMovementListener implements MouseMotionListener{
   		public void mouseMoved(MouseEvent me){
@@ -66,12 +84,12 @@ public class TyperFrame extends JFrame{
       	}
       	public void mouseDragged(MouseEvent me){
       		
+      		//System.out.println(getSize());
       	}
       	public void mouseClicked(MouseEvent me){
 
       	}
   	}
-  	
   	
 
    	class KeyBoardListener implements KeyListener {
@@ -84,10 +102,38 @@ public class TyperFrame extends JFrame{
 		public void keyTyped(KeyEvent e){}
 		public void keyReleased(KeyEvent e){}
 		public void keyPressed(KeyEvent e){
-			
+
 
 		}
 
     }
 
+    public Boolean getIsHome(){
+    	return isHome;
+    }
+
+    public void setIsHome(Boolean b){
+    	isHome = b;
+    }
+
+    public Boolean getGameStart(){
+    	return gameStart;
+    }
+
+    public void setGameStart(Boolean b){
+    	gameStart = b;
+    }
+
+    public int getGameSize(){
+    	return gameSize;
+    }
+
+    public void setGameSize(int s){
+    	gameSize = s;
+    }
+
 }
+
+
+
+
