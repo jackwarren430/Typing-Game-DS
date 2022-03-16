093abbe40f28b4d98e4e0fed48572f00d11857fc
jackwarren430 <90930942+jackwarren430@users.noreply.github.com>
2022-03-16 09:10:38 -0400

    commit

--- GameComponent.java
+++ GameComponent.java
@@ -4,17 +4,19 @@ import java.awt.geom.*;
 import java.lang.Math;
 
 public class GameComponent extends JComponent{
-	private String[] wordArr;
+	private String[] wordArr = {"blam", "kablooey", "whabul"};
 	private int gameSize;
 
-	public GameComponent(String[] wordArr){
-		this.wordArr = wordArr;
+	public GameComponent(){
 		this.gameSize = wordArr.length;
 	}
 
 	public void paintComponent(Graphics g){
 		Graphics2D pen = (Graphics2D) g;
-		pen.drawString("wham", 200, 200);
+		pen.drawString("wham", 400, 200);
+		Container c = this.getTopLevelAncestor();
+		TyperFrame t = (TyperFrame)c;
+		t.getFrameSize();
 	}
 
 
--- TyperFrame.java
+++ TyperFrame.java
@@ -15,6 +15,8 @@ public class TyperFrame extends JFrame{
 
 	private JFrame thisFrame = this;
 
+	private GameComponent game;
+
 	public TyperFrame(){
 		this.setSize(WIDTH, HEIGHT + 25);
 		this.setTitle("Typing Wizard");
@@ -29,14 +31,38 @@ public class TyperFrame extends JFrame{
 
   	public void addComponents(){
 
+  		game = new GameComponent();
+  		this.add(game);
+
 		t = new Timer(10, new MovementListener());
 		this.addMouseMotionListener(new MouseMovementListener());
+		this.addKeyListener(new KeyBoardListener());
   		this.setFocusable(true);
   	}
 
+  	public int[] getFrameSize(){
+  		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+  		pack(); // Need this, otherwise insets() show as 0.  
+		int scrW = (int)screenSize.getWidth();
+		int scrH = (int)screenSize.getHeight();
+		int innerW = scrW - getInsets().left - getInsets().right;
+		int innerH = scrH - getInsets().top - getInsets().bottom;
+  		// Need to setSize(), otherwise pack() will collapse the empty JFrame
+  		setSize(scrW, scrH);
+  		int[] toReturn = {innerW, innerH};
+  		return toReturn;
+  	}
+
+  	class MovementListener implements ActionListener{
+    	public void actionPerformed(ActionEvent e){
+        	//repaint();
+        	//System.out.println("My mouse moved!");
+    	}
+   	}
+
   	class MouseMovementListener implements MouseMotionListener{
   		public void mouseMoved(MouseEvent me){
-  		
+  			
       	}
       	public void mouseDragged(MouseEvent me){
       		
@@ -46,10 +72,22 @@ public class TyperFrame extends JFrame{
       	}
   	}
   	
-  	class MovementListener implements ActionListener{
-    	public void actionPerformed(ActionEvent e){
-        	repaint();
-    	}
-   	}
+  	
+
+   	class KeyBoardListener implements KeyListener {
+   /*
+      final int UP = 38;
+         final int DOWN = 40;
+         final int LEFT = 37;
+         final int RIGHT = 39;
+   */
+		public void keyTyped(KeyEvent e){}
+		public void keyReleased(KeyEvent e){}
+		public void keyPressed(KeyEvent e){
+			
+
+		}
+
+    }
 
 }
